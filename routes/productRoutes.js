365405cfd5e38815febf38c3a27d91b7010644ad
Santosh <santosh.kr.yd.sky@gmail.com>
2026-02-13 14:45:42 +0530

    controller

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getAllProducts,
+  addProduct,
+  getProductById,
+} = require('../controllers/productController');
 
-router.get('/', (req, res) => {
-  res.send('Fetching all products');
-});
+router.get('/', getAllProducts);
 
-router.post('/', (req, res) => {
-  res.send('Adding a new product');
-});
+router.post('/', addProduct);
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  res.send(`Fetching product with ID: ${id}`);
-});
+router.get('/:id', getProductById);
 
 module.exports = router;
